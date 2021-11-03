b1f6c6dea0fcfb57be2707acfbab604a9abdc1ad
Sean May <s_may318@hotmail.com>
2021-11-03 12:44:25 -0400

    Added a component folder and a DisplayName Component to allow names to be displayed in the APP.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import DisplayName from './Components/DisplayName/DisplayName';
+
+class App extends Component{
+    constructor(props){
+      super(props);
+
+      this.state = {
+        firstName: 'Reggie',
+        lastName: 'White'
+      }
+  }
+
+
+    render(){
+      return(
+        <div>
+          <DisplayName firstName={'Reggie'} lastName={'White'}/>
+        </div>
+    )
+  }
 }
-
 export default App;
